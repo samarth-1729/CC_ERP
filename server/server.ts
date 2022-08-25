747cde921171b2645cfa446034a2a7859402f4ae
Neil-140301 <alvares.1@iitj.ac.in>
2022-08-25 21:52:06 +0530

    starter template

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
 const app = express();
 
 // dotenv
-dotenv.config()
+dotenv.config();
 
 // middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
+// routes
 
 const PORT = process.env.PORT || 8080;
 
