af52adcba3af6fa2c3b503e36c38d7b53fc355aa
Neil-140301 <alvares.1@iitj.ac.in>
2022-08-25 21:59:17 +0530

    starter template

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,8 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen font-bold text-4xl italic underline ">
       Hello cc erp
     </div>
   )
